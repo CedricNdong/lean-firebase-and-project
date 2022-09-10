0ac5ffee00c713e57524b81621dfd5d858fd3e6e
CedricNdong <juniorndong901@gmail.com>
2022-09-10 20:15:41 +0200

    create an authentification with via google an email

--- FirebaseOC-main/app/src/main/java/com/ocr/firebaseoc/MainActivity.java
+++ FirebaseOC-main/app/src/main/java/com/ocr/firebaseoc/MainActivity.java
@@ -1,12 +1,24 @@
 package com.ocr.firebaseoc;
 
+import android.content.Intent;
 import androidx.annotation.Nullable;
 
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.snackbar.Snackbar;
 import com.ocr.firebaseoc.databinding.ActivityMainBinding;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
+
+    private static final int RC_SIGN_IN = 123;
+
 
     @Override
     ActivityMainBinding getViewBinding() {
@@ -16,6 +28,74 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupListeners();
+
     }
+
+    private void setupListeners(){
+        // Login Button
+        binding.loginButton.setOnClickListener(view -> {
+            startSignInActivity();
+        });
+    }
+
+    private void startSignInActivity(){
+
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.EmailBuilder().build()
+        );
+                Collections.singletonList(new AuthUI.IdpConfig.EmailBuilder().build());
+
+        // Launch the activity
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo_auth)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+
+
+    // Method that handles response after SignIn Activity close
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // SUCCESS
+            if (resultCode == RESULT_OK) {
+                showSnackBar(getString(R.string.connection_succeed));
+            } else {
+                // ERRORS
+                if (response == null) {
+                    showSnackBar(getString(R.string.error_authentication_canceled));
+                } else if (response.getError()!= null) {
+                    if(response.getError().getErrorCode() == ErrorCodes.NO_NETWORK){
+                        showSnackBar(getString(R.string.error_no_internet));
+                    } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                        showSnackBar(getString(R.string.error_unknown_error));
+                    }
+                }
+            }
+        }
+    }
+    // Show Snack Bar with a message
+    private void showSnackBar( String message){
+        Snackbar.make(binding.mainLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
 
 }
